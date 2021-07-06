d557e759cf392b4926037c61b086f2283dfdce4f
sanjayganni <86920619+sanjayganni@users.noreply.github.com>
2021-07-06 13:34:11 +0100

    Update getemp.py

--- getemp.py
+++ getemp.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def home():
-return render_template('getemp.html')
+         return render_template('getemp.html')
 if __name__ =='__main__':
-app.run(host='0.0.0.0',port=8080,debug=True)
+         app.run(host='0.0.0.0',port=8080,debug=True)
